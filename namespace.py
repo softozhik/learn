f6a3de73da1a79a09ac3a54e948be5881fe6678e
softozhik <softozhik@live.ru>
2018-11-06 18:56:55 +0300

    расставил пробелы перед скобками и знаками

--- namespace.py
+++ namespace.py
@@ -1,24 +1,25 @@
-def cr (ns,arg):
-    if ns=='global':
-        gl[arg]={}
-def ad (ns,arg):
-    if ns=='global':
+def cr ( ns, arg ):
+    if ns == 'global':
+        gl[arg] = {}
+    ns[arg] = {}
+def ad ( ns, arg ):
+    if ns == 'global':
         gl['var'].append(arg)
-def ge (ns,arg):
-    if ns=='global':
+def ge ( ns, arg ):
+    if ns == 'global':
         if arg in gl['var']:
             return 'global'
 
-n=int(input())
-gl={}
-gl['var']=[]
+n = int(input())
+gl = {}
+gl['var'] = []
 for i in range (n):
-    cmd,ns,arg=input().split()
-    if cmd=='add':
-        ad(ns,arg)
-    if cmd=='create':
-        cr(ns,arg)
-    if cmd=='get':
-        print(ge(ns,arg))
+    cmd, ns, arg = input().split()
+    if cmd == 'add':
+        ad( ns, arg )
+    if cmd == 'create':
+        cr( ns, arg )
+    if cmd == 'get':
+        print( ge(ns,arg) )
 #gl={'var':'a','foo':{'var':['a','b']},'doo':'c'}
 #print (gl)
